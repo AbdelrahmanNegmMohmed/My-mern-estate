68f66dd62918c79d30c203beafc175105ee45d17
AbdelrahmanNegmMohamed <abdelrahman.negm.mohamed@gmail.com>
2024-03-31 14:22:01 +0200

    create and run the server

---
+++ api/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+
+app.listen(3000,()=>{//تعريف السيرفر علي بورت 3000
+    console.log("server is running on port 30000!!");
+})
+//لو فعلت السيرفر علي هذا الوضع يحدس خطأ وهو تعديل النوع في ملف باكججسون
+
+
+
