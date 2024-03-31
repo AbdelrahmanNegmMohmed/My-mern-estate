a3b29cf6ec70b111750116b12d8014f0c4d12d61
AbdelrahmanNegmMohamed <abdelrahman.negm.mohamed@gmail.com>
2024-03-31 16:07:36 +0200

    conect to database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Conected to MongoDb");
+}
+).catch((err)=>{
+    console.log(err,"errorrrrrrr");
+})
+
+
 const app = express();
 
+
 app.listen(3000,()=>{//تعريف السيرفر علي بورت 3000
-    console.log("server is running on port 30000!!");
+    console.log("server is running on port 3000!!");
 })
 //لو فعلت السيرفر علي هذا الوضع يحدس خطأ وهو تعديل النوع في ملف باكججسون
 
