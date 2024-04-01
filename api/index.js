a4dc411cee2d6260ef9df8db4a05843569470c07
AbdelrahmanNegmMohamed <abdelrahman.negm.mohamed@gmail.com>
2024-04-01 09:19:53 +0200

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
 import  userRouter from "./route/user.route.js" // 4 (1)
+import authRouter from "./route/auth.route.js";
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("Conected to MongoDb");
@@ -12,6 +13,7 @@ mongoose.connect(process.env.MONGO).then(()=>{
 
 
 const app = express();
+app.use(express.json())
 
 
 app.listen(3000,()=>{//تعريف السيرفر علي بورت 3000
@@ -20,4 +22,5 @@ app.listen(3000,()=>{//تعريف السيرفر علي بورت 3000
 //لو فعلت السيرفر علي هذا الوضع يحدس خطأ وهو تعديل النوع في ملف باكججسون
 
 app.use('/api/user',userRouter) // 5 (1)
+app.use('/api/auth',authRouter)
 
