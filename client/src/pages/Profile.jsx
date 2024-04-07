cd2a952bec55820241bff55d9040e15970f9a42d
AbdelrahmanNegmMohamed <abdelrahman.negm.mohamed@gmail.com>
2024-04-07 17:53:58 +0200

    complete upload listing images functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -19,6 +19,8 @@ sidnoutuserStart,
 sidnoutuserSuccess,
 sidnoutuserFailure,
 } from "../Redux/user/userslise.js";
+import { Link } from 'react-router-dom';
+
 
 export default function Profile() {
   const fileRef = useRef(null);
@@ -180,6 +182,9 @@ export default function Profile() {
         >
           {loading ? "loading..." : "update"}
         </button>
+        <Link className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95' to={"/create-listing"}>
+          Create Listing
+        </Link>
       </form>
       <div className="flex justify-between mt-5">
         <span
